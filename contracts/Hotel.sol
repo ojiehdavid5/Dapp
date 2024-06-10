eeb19e72e6701fa197aacb52be8b0c331e7e21d8
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-06-10 21:09:31 +0100

    contract

---
+++ contracts/Hotel.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract HotelRooom{
+    //Ether payment
+    //Modifiers
+    //Visibility
+    //Events
+    //Enums
+//
+
+    address payable public owner=0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199;
+
+
+    function() public views returns(uint){
+
+    }
+
+
+}
