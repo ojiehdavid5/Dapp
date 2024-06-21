1876eb9f9ed51c913a3cec3d77f57f3326e714be
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-06-21 01:09:48 +0100

    changing contract

---
+++ contracts/Secret.sol
@@ -0,0 +1,59 @@
+//SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+
+
+contract Ownable{
+    address owner;
+
+    modifier onlyOwner(){
+        require(msg.sender==owner,"must be the owner");
+
+        _;
+    }
+}
+
+
+contract SecretVault{
+    string secret;
+        constructor  (string memory _secret) {
+    secret=_secret;
+
+}
+
+function getSecret()  public view  returns(string memory){
+
+    return secret;
+
+
+}
+
+}
+
+
+contract MyContract is Ownable{
+
+
+    
+
+
+
+    constructor  (string memory _secret) {
+        SecretVault _secretVault= new SecretVault(_secret);
+           super;
+
+}
+
+
+function getSecret()  public view onlyOwner returns(string memory){
+
+    return secret;
+
+
+}
+}
+
+
