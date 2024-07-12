efac35c23c199675911767148ecf5802e17fd6d0
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-07-12 17:33:11 +0100

    change in contract

--- contracts/Secret.sol
+++ contracts/Secret.sol
@@ -5,81 +5,38 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
 
-
-contract Ownable{
+contract Ownable {
     address owner;
 
-    modifier onlyOwner(){
-        require(msg.sender==owner,"must be the owner");
+    modifier onlyOwner() {
+        require(msg.sender == owner, "must be the owner");
 
         _;
     }
 }
 
-
-contract SecretVault{
+contract SecretVault {
     string secret;
-        constructor  (string memory _secret) {
-    secret=_secret;
+    constructor(string memory _secret) {
+        secret = _secret;
+    }
 
-}
-
-function getSecret()  public view  returns(string memory){
-
-    return secret;
-
-
-}
-
-}
-
-
-contract MyContract is Ownable{
-
-    string secret;
-
-
-    
-
-    
-
-
-    
-
-
-
-    constructor  (string memory _secret) payable {
+    function getSecret() public view returns (string memory) {
         
-        SecretVault _secretVault= new SecretVault(_secret);
-
-
-
-        
-
-
-
-
-
-
-
-
-        
-
-
-           super;
-
-
-           
-
+        return secret;
+    }
 }
+contract MyContract is Ownable {
 
+    address secretVault;
+    constructor(string memory _secret) payable {
+        SecretVault _secretVault = new SecretVault(_secret);
+        secretVault=address(_secretVault);
 
-function getSecret()  public view onlyOwner returns(string memory ){
+        super;
+    }
 
-    return secret;
-
-
+    function getSecret() public view onlyOwner returns (string memory) {
+        return SecretVault(secretVault).getSecret();
+    }
 }
-}
-
-
