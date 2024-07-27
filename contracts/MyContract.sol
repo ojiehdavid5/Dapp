c9f979dd31946fbec8c361d52a9488393244cc42
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-07-27 21:55:49 +0100

    change in contract

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,22 +1,22 @@
-// //SPDX-License-Identifier: Unlicense
-// pragma solidity ^0.8.0;
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
 
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
 
 
  
 
 
-//  contract MyContract{
-//       address payable owner=0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199 ;
+ contract MyContract{
+      address payable owner=0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199 ;
 
 
       
 
 
-//     constructor(){
-//         owner=msg.sender;
+    constructor(){
+        owner=msg.sender;
 
 
 
@@ -25,43 +25,44 @@
 
 
         
-//     }
+    }
 
 
-//     uint[] public numbers=[1,2,3,4,5,6,7,8,9,10];
+    uint[] public numbers=[1,2,3,4,5,6,7,8,9,10];
 
 
-//     function countEvenNumbers()public view returns(uint){
+    function countEvenNumbers()public view returns(uint){
 
-//         uint count=0;
-//         for(uint i=0; i<numbers.length; i++){
-//             if(isEvenNumber(numbers[i])){
-//                 count++;
+        uint count=0;
+        for(uint i=0; i<numbers.length; i++){
+            if(isEvenNumber(numbers[i])){
+                count++;
             
-//         }
-//     }
+        }
+    }
 
-//     return count;
+    return count;
 
-//     }
+    }
 
     
 
-//     function isEvenNumber(uint _number)public view returns(bool){
+    function isEvenNumber(uint _number)public view returns(bool){
 
 
-// if(_number % 2 == 0){
-//     return true;
-// }else{
-//     return false;
-// }
-//     }
+if(_number % 2 == 0){
 
-// // function isOwner() public   view  returns(bool){
+    return true;
+}else{
+    return false;
+}
+    }
 
-//     //     return(msg.sender == owner);
+// function isOwner() public   view  returns(bool){
 
-//     //  }
+    //     return(msg.sender == owner);
+
+    //  }
 
 
-//  }
+ }
