66b45084aa0dcf737cbde4a5388108b9353b50ee
ojieh (chuks) david <ojiehdavid5@gmail.com>
2024-08-08 22:52:25 +0100

    Update FundMe.sol

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,43 +1,33 @@
- //SPDX-License-Identifier: GPL-3.0
+//SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
- contract FundMe{
+import "./PriceConverter.sol";
 
-    uint256 public minimumUsd=50 * 1e18;
-    function fund( uint256 _number)public payable returns(uint256){
-        
-require(getConversionRate(msg.value) >=minimumUsd,"no much money");
+contract FundMe {
+    using PriceConverter for uint256;
 
+    uint256 public minimumUsd = 50 * 1e18;
+    address[] public funders;
+    mapping(address => uint256) public AddressToAmountFunded;
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= minimumUsd, "no much money");
+        funders.push(msg.sender);
+        AddressToAmountFunded[msg.sender] += msg.value;
     }
+    function withdraw()public {
+        for(uint256 funderIndex=0; funderIndex<funders.length; funderIndex++){
+            address funder=funders[funderIndex];
+            AddressToAmountFunded[funder]=0;
 
-    function getPrice() public  view returns(uint256) {
+        }
 
-
-        //ABI
-        //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        AggregatorV3Interface priceFeed= AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-
-
-        (
-            /* uint80 roundID */,
-            int answer,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
-        ) = priceFeed.latestRoundData();
-
-return uint256(answer* 1e10);
-        
+        funders=new address[](0);
+        ///how to send ether to a particular address
+        //transfer
+        //send
+        //call
     }
-
-    function getConversionRate(uint ethAmount) public view returns(uint256){
-        //ABI
-        //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        uint256 ethPrice=getPrice();
-        uint256 ethAmountInUsd=(ethPrice*ethAmount)/1e18;
-        return ethAmountInUsd;
-
-    }
- }
+}
