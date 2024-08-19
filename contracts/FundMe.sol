e093962535a592aa92f7544fc27176f7565bb931
ojieh (chuks) david <ojiehdavid5@gmail.com>
2024-08-19 01:06:03 +0100

    Update FundMe.sol

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -8,6 +8,10 @@ import "./PriceConverter.sol";
 contract FundMe {
     using PriceConverter for uint256;
 
+
+
+
+
     uint256 public constant  minimumUsd = 50 * 1e18;
     address[] public funders;
     mapping(address => uint256) public AddressToAmountFunded;
