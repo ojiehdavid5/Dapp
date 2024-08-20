11d6004e00a94f1acf688d0978309d5cb71d65e7
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-08-20 01:17:49 +0100

    hardhat issues

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,35 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
+const JSON_RPC=process.env.JSON_RPC;
+const PRIVATE_KEY=process.env.PRIVATE_KEY;
+const API_KEY=process.env.API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork:"hardhat",
+
   solidity: "0.8.19",
   paths:{
     artifacts:"./src/artifacts",
   },
 
   networks: {
-hardhat:{
-  chainId:1337,
+localhost:{
+  url:JSON_RPC,
+  // account:[PRIVATE_KEY],
+  chainId:31337,
 },
+// etherscan: {
+//   // Your API key for Etherscan
+//   // Obtain one at https://etherscan.io/
+//   apiKey: API_KEY
+// },
+// sourcify: {
+//   // Disabled by default
+//   // Doesn't need an API key
+//   enabled: true
+// }
 
   }
 };
