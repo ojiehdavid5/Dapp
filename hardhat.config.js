4da93e26e7ff4b976481e9f9ac2f3e238386e789
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-08-27 23:13:01 +0100

    change in contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-verify");
 require("dotenv").config();
+require('hardhat-gas-reporter');
+require('solidity-coverage');
 const JSON_RPC=process.env.JSON_RPC;
 const PRIVATE_KEY=process.env.PRIVATE_KEY;
 const API_KEY=process.env.API_KEY;
+const COINMARKETCAP_API_KEY=process.env.COINMARKETCAP_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -21,15 +24,22 @@ localhost:{
   chainId:31337,
 },
 // etherscan: {
-//   // Your API key for Etherscan
-//   // Obtain one at https://etherscan.io/
-//   apiKey: API_KEY
-// },
-// sourcify: {
-//   // Disabled by default
-//   // Doesn't need an API key
-//   enabled: true
-// }
+// //   // Your API key for Etherscan
+// //   // Obtain one at https://etherscan.io/
+//    apiKey: API_KEY,
+//  }
 
+  },
+  // gasReporter:{
+  //   enabled: true,
+  //   outputFile:"gas-report.txt",
+  //   currency:"USD",
+  //   coinmarketcap:COINMARKETCAP_API_KEY,
+  //   token:"MATIC"
+  // },
+  gasReporter: {
+    currency: 'NGN',
+    L1: "binance",
+    coinmarketcap: COINMARKETCAP_API_KEY,
   }
 };
