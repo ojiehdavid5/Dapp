aef92cb13918a3a864156557ba0c0a6034cb3bc4
ojieh (chuks) david <ojiehdavid5@gmail.com>
2025-01-05 17:35:32 +0100

    Update PriceConverter.sol

--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -7,6 +7,8 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 library PriceConverter{
     function getPrice() internal  view returns(uint256) {
         //ABI
+
+
         //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
         AggregatorV3Interface priceFeed= AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
 
